b3e917e4dfb0fc3f843b5ab5b1d5e29744bc5a28
ryaneof <ryaneof@gmail.com>
2014-01-04 02:27:11 +0800

    2: convert indetations to spaces..

--- config/acl.php
+++ config/acl.php
@@ -5,18 +5,18 @@
 
 $acl = array();
 
-// ['controller']['method'] 				Role Ids which have permission to visit, joined by comma
+// ['controller']['method']         Role Ids which have permission to visit, joined by comma
 
 // In this case, users with role id 1 or 2,3,4,6 will be able to access uri home/*, 
 // but only users with role id 1 are able to access uri home/protected
 
-$acl['home']['*']							= '1,2,3,4,6'; 	// http://your.site.url/home
-$acl['home']['protected']					= '1';			// http://your.site.url/home/protected
+$acl['home']['*']                   = '1,2,3,4,6';  // http://your.site.url/home
+$acl['home']['protected']           = '1';          // http://your.site.url/home/protected
 
-$acl['project']['*']						= '1';
+$acl['project']['*']                = '1';
 
-$acl['user']['*']							= '1,2';		// http://your.site.url/user
-$acl['user']['list']['*']					= '1';			// http://your.site.url/user/list/
-$acl['user']['group']['admin']				= '1';			// http://your.site.url/user/group/admin
+$acl['user']['*']                   = '1,2';        // http://your.site.url/user
+$acl['user']['list']['*']           = '1';          // http://your.site.url/user/list/
+$acl['user']['group']['admin']      = '1';          // http://your.site.url/user/group/admin
 
 
